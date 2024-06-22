80badb61a4f77571afe6bcc2aefb2d13e96e680f
comugong <165180439+comugong@users.noreply.github.com>
2024-06-22 19:34:45 +0900

    Update VanController.java

--- VanController.java
+++ VanController.java
@@ -11,23 +11,23 @@ package com.web.p7; //컨트롤러
     @Autowired
     private MemberRepository mrep;
 
-    @GetMapping("/member/list")
+    @GetMapping("/member/list") //회원리스트
     public String memberList(Model mo) {
     mo.addAttribute("arr",mrep.findAll());
     return "memberList";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/login") //로그인
     public String login() {
     return "login";
     }
 
-    @GetMapping("/member/register")
+    @GetMapping("/member/register") //회원가입
     public String memberRegister() {
       return "memberRegister";
     }
 
-    @GetMapping("/member/insert")
+    @GetMapping("/member/insert") //회원가입 세부사항항
     public String memberInsert(String id, String pw, String name, String phone, RedirectAttributes re) {
       if ( mrep.existsById(id)) {
          re.addAttribute("msg", id+"는 이미 사용되고 있는 아이디입니다.");
@@ -44,14 +44,14 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/popup")
+    @GetMapping("/popup") //리다이렉션에 필요한 팝업
     public String popup(String msg, String url, Model mo) {
       mo.addAttribute("msg", msg);
       mo.addAttribute("url", url);
       return "popup";
     }
 
-    @GetMapping("/login/check")
+    @GetMapping("/login/check") //로그인 시 데이터베이스와 대조
     public String loginCheck(HttpSession se, String id, Model mo, RedirectAttributes re) {
       if(mrep.existsById(id)) {
          se.setAttribute("id", id);
@@ -64,13 +64,13 @@ package com.web.p7; //컨트롤러
       }
     }
 
-    @GetMapping("/menu")
+    @GetMapping("/menu") //메인화면면
     public String menu(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       return "menu";
     }
 
-    @GetMapping("/myinfo")
+    @GetMapping("/myinfo") //마이페이지
     public String myinfo(HttpSession se, Model mo) {
       String id = (String)se.getAttribute("id");
       Member me = mrep.findById(id).get();
@@ -78,7 +78,7 @@ package com.web.p7; //컨트롤러
       return "myinfo";
     }
 
-    @GetMapping("/myinfo/update")
+    @GetMapping("/myinfo/update") //회원이 정보 변경할 시 필요함
     public String myinfoUpdate(HttpSession se, String password, String username, String email, RedirectAttributes re) {
       String id = (String)se.getAttribute("id");
       if(mrep.updateMember(id, password, username, email) == 0)
@@ -90,14 +90,14 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/cinemainfo")
+    @GetMapping("/cinemainfo") //관리자페이지 - 극장정보
     public String cinemainfo(Model mo) {
       long a = mrep.countMember();
-      mo.addAttribute("mcount", fo.format(a)+"명");
+      mo.addAttribute("mcount", fo.format(a)+"명"); //총 가입 회원 수 표시
       return "cinemainfo";
     }
 
-    @GetMapping("/logout")
+    @GetMapping("/logout") //로그아웃
     public String logout(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       se.invalidate();
