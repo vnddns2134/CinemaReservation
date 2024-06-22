2b65b4532bd117b81cedcd3f80d5486e4e69c2d1
comugong <165180439+comugong@users.noreply.github.com>
2024-06-22 20:03:53 +0900

    Entity_ver.2

--- Member.java
+++ Member.java
@@ -6,8 +6,8 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Member {
-@Id public String id;
-public String password;
-public String username;
-public String email;
+@Id public String id; //아이디
+public String password; //비밀번호
+public String username; //닉네임
+public String email; //이메일
 }
