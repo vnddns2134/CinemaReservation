c1384e280a2e38f4f2842f44e4cf58411be1cdce
comugong <165180439+comugong@users.noreply.github.com>
2024-06-22 20:01:38 +0900

    Repository

--- MemberRepository.java
+++ MemberRepository.java
@@ -9,6 +9,19 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     @Modifying
     @Query("update Member set password=?2, username=?3, email=?4 where id=?1")
     int updateMember(String id, String password, String username, String email);
+    
+    @Transactional //비밀번호 초기화
+    @Modifying
+    @Query("update Member set password=concat("
+            + "CHAR(FLOOR(65 + (RAND() * 26))), "
+            + "CHAR(FLOOR(65 + (RAND() * 26))), "
+            + "CHAR(FLOOR(97 + (RAND() * 26))), "
+            + "CHAR(FLOOR(97 + (RAND() * 26))), "
+            + "CHAR(FLOOR(48 + (RAND() * 10))), "
+            + "CHAR(FLOOR(48 + (RAND() * 10))) "
+            + "
+    where id=?1")
+    int resetPassword(String id);
 
     @Query("select count(id) from Member")
     long countMember();
