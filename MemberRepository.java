6136ed4ddea5ac6ca9941ae79bb47655acb6c86f
comugong <165180439+comugong@users.noreply.github.com>
2024-06-22 20:03:01 +0900

    Repository_ver.2

--- MemberRepository.java
+++ MemberRepository.java
@@ -3,7 +3,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import jakarta.transaction.Transactional;
-
+ 
 public interface MemberRepository extends JpaRepository<Member, String> {
     @Transactional
     @Modifying
