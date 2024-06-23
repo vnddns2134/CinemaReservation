3d00bfe97a066fb657ec4946c387793f7554a004
youzting <158013348+youzting@users.noreply.github.com>
2024-06-24 06:47:25 +0900

    Create application.java

---
+++ application.java
@@ -0,0 +1,13 @@
+package com.web.p8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P81Application.class, args);
+	}
+
+}
