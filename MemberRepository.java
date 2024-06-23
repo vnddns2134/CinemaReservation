4708626e4170b224f990d63a71ede5d9452c08fb
comugong <165180439+comugong@users.noreply.github.com>
2024-06-24 06:42:13 +0900

    Repository_ver.fin

--- MemberRepository.java
+++ MemberRepository.java
@@ -5,20 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import jakarta.transaction.Transactional;
 
 public interface MemberRepository extends JpaRepository<Member, String>{
-	@Transactional
-    @Modifying
-    @Query("update Member set password=?2, username=?3, email=?4 where id=?1")
-    int updateMember(String id, String password, String username, String email);
+	@Query("select m.password from Member m where m.id=?1")
+	String findPasswordById(String id);
 	
 	@Transactional
-	@Modifying
-	@Query(value = "UPDATE Member SET pw = CONCAT("
-            + "CHAR(FLOOR(65 + (RAND() * 26))), "
-            + "CHAR(FLOOR(65 + (RAND() * 26))), "
-            + "CHAR(FLOOR(97 + (RAND() * 26))), "
-            + "CHAR(FLOOR(97 + (RAND() * 26))), "
-            + "CHAR(FLOOR(48 + (RAND() * 10))), "
-            + "CHAR(FLOOR(48 + (RAND() * 10))) "
-            + ") WHERE id = ?1", nativeQuery = true)
-    int resetPassword(String id);
+    @Modifying
+    @Query("update Member set password=?2, email=?3 where id=?1")
+    int updateMember(String id, String password, String email);
 }
