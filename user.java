8e571bcda34d0318f7a619ccbc290dd4feb93993
youzting <158013348+youzting@users.noreply.github.com>
2024-06-24 05:56:51 +0900

    Create user.java

---
+++ user.java
@@ -0,0 +1,46 @@
+package com.web.p8;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class user {
+    @Id
+    private String id;
+    private String password;
+    private String username;
+    private String email;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
