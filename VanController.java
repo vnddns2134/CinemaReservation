bbf69215cf5a6c57f370b930f94124e4d4a1063e
comugong <165180439+comugong@users.noreply.github.com>
2024-06-23 19:11:03 +0900

    Controller_ver.7

--- VanController.java
+++ VanController.java
@@ -14,17 +14,17 @@ package com.web.p7; //컨트롤러
     @Autowired
     private MovieRepository morep;
 
-    @GetMapping("/login") //로그인
+    @GetMapping("/login") // 로그인
     public String login() {
     return "login";
     }
 
-    @GetMapping("/logup") //회원가입
+    @GetMapping("/logup") // 회원가입
     public String logup() {
       return "logup";
     }
 
-    @GetMapping("/logup/insert") //회원가입 세부사항
+    @GetMapping("/logup/insert") // 회원가입 세부사항
     public String logupInsert(String id, String pw, String name, String phone, RedirectAttributes re) {
       if ( mrep.existsById(id)) {
          re.addAttribute("msg", id+"는 이미 사용되고 있는 아이디입니다.");
@@ -41,14 +41,14 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/popup") //리다이렉션에 필요한 팝업
+    @GetMapping("/popup") // 리다이렉션에 필요한 팝업
     public String popup(String msg, String url, Model mo) {
       mo.addAttribute("msg", msg);
       mo.addAttribute("url", url);
       return "popup";
     }
 
-    @GetMapping("/login/check") //로그인 시 데이터베이스와 대조
+    @GetMapping("/login/check") // 로그인 시 데이터베이스와 대조
     public String loginCheck(HttpSession se, String id, Model mo, RedirectAttributes re) {
       if(mrep.existsById(id)) {
          se.setAttribute("id", id);
@@ -61,20 +61,20 @@ package com.web.p7; //컨트롤러
       }
     }
 
-    @GetMapping("/home") //메인화면
+    @GetMapping("/home") // 메인화면
     public String menu(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       return "home";
     }
 
-    @GetMapping("/member") //관리자페이지
+    @GetMapping("/member") // 관리자페이지
     public String member(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       mo.addAttribute("arr",mrep.findAll());
       return "member";
     }
 
-    @GetMapping("/movieList") //영화목록
+    @GetMapping("/movieList") // 영화목록
     public String movieList(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       return "movieList";
@@ -104,7 +104,7 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/mypage") //마이페이지
+    @GetMapping("/mypage") // 마이페이지
     public String myinfo(HttpSession se, Model mo) {
       String id = (String)se.getAttribute("id");
       Member me = mrep.findById(id).get();
@@ -112,7 +112,7 @@ package com.web.p7; //컨트롤러
       return "mypage";
     }
 
-    @GetMapping("/mypage/update") //회원이 정보 변경할 시 필요함
+    @GetMapping("/mypage/update") // 회원이 정보 변경할 시 필요함
     public String myinfoUpdate(HttpSession se, String password, String username, String email, RedirectAttributes re) {
       String id = (String)se.getAttribute("id");
       if(mrep.updateMember(id, password, username, email) == 0)
@@ -124,7 +124,7 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/password/reset") //비밀번호 모를 시 비밀번호 초기화
+    @GetMapping("/password/reset") // 비밀번호 모를 시 비밀번호 초기화
     public String passwordReset(HttpSession se, String password, RedirectAttributes re) {
       String id = (String)se.getAttribute("id");
       String result = mrep.resetPassword(id);
@@ -134,7 +134,7 @@ package com.web.p7; //컨트롤러
       return "redirect:/popup";
     }
 
-    @GetMapping("/logout") //로그아웃
+    @GetMapping("/logout") // 로그아웃
     public String logout(HttpSession se, Model mo) {
       mo.addAttribute("id", se.getAttribute("id"));
       se.invalidate();
